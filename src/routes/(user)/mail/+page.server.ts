387cbc00811974bd13397e47bbbfbe7d4650043d
Jasper M-W <contact@jmw.nz>
2023-01-27 10:09:14 +1300

    Switch to mailjet

--- src/routes/(user)/mail/+page.server.ts
+++ src/routes/(user)/mail/+page.server.ts
@@ -1,11 +1,13 @@
 import type { Actions, RequestEvent } from "@sveltejs/kit";
-import {default as formData} from "form-data";
-import {default as Mailgun} from "mailgun.js";
+import { default as Mailjet, type Contact } from "node-mailjet";
+import {v4 as uuidv4} from 'uuid';
 
-const MAILGUN_API_ROOT = "https://api.mailgun.net/v3";
-const mailgun = new Mailgun(formData);
+import { env } from "$env/dynamic/private";
 
-const client = mailgun.client({ username: 'api', key: 'YOUR_API_KEY' || '' });
+const mailjet = new Mailjet({
+	apiKey: env.SECRET_MJ_API_KEY,
+	apiSecret: env.SECRET_MJ_API_Secret,
+});
 
 // (async () => {
 // 	try {
@@ -29,11 +31,36 @@ export const actions: Actions = {
 		const data = await request.formData();
 		const email = data.get('email');
 
-		let user = await fetch(`{MAILGUN_API_ROOT}/lists/LIST@YOUR_DOMAIN_NAME/members`,{method: "POST"});
+		let success = true;
+
+		try {
+			const nameID = uuidv4();
+			const newMember: Contact.PostContactResponse = (await mailjet.post("contact", { version: 'v3' })
+				.request({
+					IsExcludedFromCampaigns: "false",
+					Name: nameID,	  
+					Email: email
+				})).body;
+			console.log('Created member',nameID,"Response:", newMember);
+
+			let contactID = newMember["Data"][0]["ID"];
+
+			const request = mailjet
+				.post("listrecipient", { 'version': 'v3' })
+				.request({
+					"ContactID": "987654321",
+					"ContactAlt": "passenger@mailjet.com",
+					"ListID": "123456",
+					"ListAlt": "abcdef123"
+				})
+
+		} catch (error) {
+			console.error(error);
+			success = false;
+		}
 
 		return {
-			status,
-			body: { error }
+			success,
 		};
 	},
 }
