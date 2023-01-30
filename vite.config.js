8a74360bbc05da1b1f4165d29a1cb934d8f3ae30
Jasper M-W <contact@jmw.nz>
2023-01-31 01:57:45 +1300

    Temp hacks to fix non mail

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), viteCommonjs()],
+	build:{
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		}
+	},
+	optimizeDeps: {
+		include: ["html-minifier","uglify-js"]
+	}
 };
 
 export default config;
